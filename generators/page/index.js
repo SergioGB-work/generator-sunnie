fac2e2a6d70299f9f6b143686a2dd81e4e47d5ff
Sergio González Barrios <sgonzalez@aventia.com>
2016-12-11 22:53:13 +0100

    Now you can select wehere do you want to add your new page and the position of it

--- generators/page/index.js
+++ generators/page/index.js
@@ -1,73 +1,214 @@
 var fs=require('fs');
+var path = [];
+var lastLevel = [];
+var positionToAdd=0;
 
 module.exports = require('yeoman-generator').Base.extend({
 	
-  'initializing': function(){
-	  var dirLayoutsBundles = fs.readdirSync('../../../bundles/src/layouts/');
-	  var dirLayoutsPlugins = fs.readdirSync('../../layouts/');
-	  this.choicesLayout=[];
-	  
-	  for(var i in dirLayoutsBundles){
-		 var layoutName = dirLayoutsBundles[i].split('.')
-		 var layout = {name:layoutName[0], value: layoutName[0]}
-		 this.choicesLayout.push(layout); 
-		  
-	  }
-	  
-	  for(var i in dirLayoutsPlugins){
-		 var layoutName = dirLayoutsPlugins[i].split('.')
-		 var layout = {name:layoutName[0], value: layoutName[0]}
-		 this.choicesLayout.push(layout); 
-		  
-	  }	  
-	   
-	   return this.choicesLayout;
-  },	
-	
-  'prompting' : function () {
+	initializing: function(){
+		var dirLayoutsBundles = fs.readdirSync('../../../bundles/src/layouts/');
+		var dirLayoutsPlugins = fs.readdirSync('../../layouts/');
 
-    return this.prompt([{
-      type    : 'input',
-      name    : 'page_name',
-      message : 'Your page name',
-      default : this.appname // Default to current folder name
-    },	{
-      type    : 'input',
-      name    : 'metatitle',
-      message : 'Your page meta title',
-      default : this.appname // Default to current folder name
-    },{
-      type    : 'input',
-      name    : 'metakeywords',
-      message : 'Your page meta keyworks(separated with ,)',
-      default : this.appname // Default to current folder name
-    },{
-      type    : 'input',
-      name    : 'metadescription',
-      message : 'Your page description',
-      default : this.appname // Default to current folder name
-    },{
-      type    : 'list',
-      name    : 'layout',
-      message : 'Choose your layout from the list:',
-	  choices: this.choicesLayout
-	  
-    }]).then(function (answers) {
-	  this.props = answers
-      this.log('app name', answers.name);
-    }.bind(this));
-
-
-  },
-
-  'writing' : function () {
+		this.choicesLayout=[];
 		
+		for(var i in dirLayoutsBundles){
+			var layoutName = dirLayoutsBundles[i].split('.')
+			var layout = {name:layoutName[0], value: layoutName[0]}
+			this.choicesLayout.push(layout); 
+		  
+		}
+
+		for(var i in dirLayoutsPlugins){
+			var layoutName = dirLayoutsPlugins[i].split('.')
+			var layout = {name:layoutName[0], value: layoutName[0]}
+			this.choicesLayout.push(layout); 
+		  
+		}	  
+
+		var siteMap = this.readFileAsString('./sitemap.json')
+		
+		siteMap = JSON.parse(siteMap);
+		this.siteMap = [];
+		
+		this.siteMap.push({name:'Add to this level',value:0});
+		
+		for(var i=1; i <= siteMap.pages.length;i++){
+			var page = {name:'('+i+')'+siteMap.pages[i-1].name,value:i}
+			this.siteMap.push(page);
+		}
+		
+		
+		return this.choicesLayout,this.siteMap;
+	},
+	
+	askCommon: function () {
+		
+		done = this.async();
+		
+		var pageNameQuestion = {
+		  type    : 'input',
+		  name    : 'page_name',
+		  message : 'Your page name',
+		  default : this.appname // Default to current folder name
+		};
+		var pageSrcQuestion = {
+		  type    : 'input',
+		  name    : 'src',
+		  message : 'Your page file(example: home(without .jade) )'
+		};		
+		var pageUrlQuestion = {
+		  type    : 'input',
+		  name    : 'url',
+		  message : 'Your page URL(example: /home )',
+		  default : this.appname // Default to current folder name
+		};	
+		var pageMetatitleQuestion= {
+		  type    : 'input',
+		  name    : 'metatitle',
+		  message : 'Your page meta title',
+		  default : this.appname // Default to current folder name
+		};
+		var pageMetakeywordsQuestion= {
+		  type    : 'input',
+		  name    : 'metakeywords',
+		  message : 'Your page meta keyworks(separated with ,)',
+		  default : this.appname // Default to current folder name
+		};
+		var pageMetadescriptionQuestion= {
+		  type    : 'input',
+		  name    : 'metadescription',
+		  message : 'Your page description',
+		  default : this.appname // Default to current folder name
+		};
+		var pageLayoutQuestion= {
+		  type    : 'list',
+		  name    : 'layout',
+		  message : 'Choose your layout from the list:',
+		  choices: this.choicesLayout
+		  
+		}
+		var pageSitemapFirstLevelQuestion= {
+		  type    : 'list',
+		  name    : 'firstLevelSitemap',
+		  message : 'where do you want to add your page?',
+		  choices: this.siteMap	  
+		  
+		};	
+		
+		this.prompt([pageNameQuestion,pageSrcQuestion,pageUrlQuestion,pageMetatitleQuestion,pageMetadescriptionQuestion,pageMetakeywordsQuestion,pageLayoutQuestion,pageSitemapFirstLevelQuestion]).
+		
+		then(function (props) {
+			
+			this.props = props;
+			
+			position = props.firstLevelSitemap;
+			
+			if(position==0){
+				lastLevel = this.siteMap;
+				this.addToPosition();
+			}
+			
+			else{
+				path.push(position-1);
+				this.nextLevel(path);
+			}
+			
+		}.bind(this));  	
+	},
+	
+	nextLevel : function(path){
+		var pages = [];
+		
+		var siteMap = this.readFileAsString('./sitemap.json');
+		siteMap = JSON.parse(siteMap);
+		var aux = siteMap.pages;
+		
+		for(var i in path){
+			
+			if(aux[path[i]].childs){
+				aux = aux[path[i]].childs;
+			}
+			else{
+				aux = [];
+			}
+		}
+		
+		pages.push({name:'(0)Add to this level',value:0});
+		
+		for(var i=1; i <= aux.length;i++){
+			var page = {name:'('+i+')' + aux[i-1].name,value:i}
+			pages.push(page);
+		}		
+		
+		lastLevel = pages;
+		
+		var levelQuestion = {
+			type    : 'list',
+			name    : 'position',
+			message : 'where do you want to add your page?',
+			choices: pages	    
+		};	
+		
+		
+		this.prompt(levelQuestion).
+		
+		then(function (props) {
+		
+			if(props.position==0){
+				this.addToPosition();
+			}
+			else{
+				path.push(props.position-1);
+				this.nextLevel(path);
+			}
+		
+		}.bind(this));
+		
+	},
+
+	addToPosition : function(pos){
+		
+		var positions = []
+		
+		lastLevel.shift();
+		
+		positions.push({name: 'At to the beginning',value:'0'});
+		
+		for(var i=1 ; i < lastLevel.length; i++){
+			
+			var item = lastLevel[i-1].name.split(')');
+			
+			positions.push({name:'After '+item[1],value: i});
+		}
+		
+		positions.push({name: 'At to the end',value:lastLevel.length});
+		
+		var position = {
+			type    : 'list',
+			name    : 'position',
+			message : 'What position do you want to add your page in',
+			choices: positions   
+		};		
+		
+		this.prompt(position).
+		
+		then(function (props) {
+			positionToAdd = props.position;
+			this.writing();
+		}.bind(this));		
+	},
+
+	writing : function () {
+		
+		/************* CREATING PAGE ***********/	
 		var pageName = this.props.page_name;
+		var pageSrc = this.props.src || pageName + '.jade';
+		var url = this.props.url;
 		var metatitle = this.props.metatitle;
 		var metakeywords = this.props.metakeywords;
 		var metadescription = this.props.metadescription;
 		var layout = this.props.layout;
-		var path='';
+		var pathLayout='';
 		var content=''; 
 		
 		content='extends /layouts/'+layout+'\n\n';
@@ -77,16 +218,16 @@ module.exports = require('yeoman-generator').Base.extend({
 		content += '	meta(name="description",content="'+metadescription+'")\n\n';
 			
 		if(this.fs.exists("../../../bundles/src/layouts/" + layout + '.jade')){
-			path = "../../../bundles/src/layouts/" + layout + '.jade';
+			pathLayout = "../../../bundles/src/layouts/" + layout + '.jade';
 		}
 		else if(this.fs.exists("../../layouts/" + layout + '.jade')){
-			path = "../../layouts/" + layout + '.jade';
+			pathLayout = "../../layouts/" + layout + '.jade';
 		}
 		else{
 			this.log("Error, Layout not found");
 		}
 		
-		file = this.readFileAsString(path);
+		file = this.readFileAsString(pathLayout);
 		
 		var blocks = [];
 		
@@ -105,10 +246,42 @@ module.exports = require('yeoman-generator').Base.extend({
 			}
 			
 		}
-		this.fs.write(
-			 this.destinationPath(pageName+'.jade'),
+		fs.writeFile(pageName+'.jade',
 			 content
-		);		
+		);
+
+		/****************** ADDING TO SITEMAP *****************/
+		var siteMap = this.readFileAsString('./sitemap.json');
+		var newPage = {"name": pageName,"url": url,"src": pageSrc+'.html',"childs": []};
+		
+		var pages = JSON.parse(siteMap).pages;
+		
+		newSitemap = pushJson(pages,newPage,path,path.length,0,positionToAdd);
+		siteMap = JSON.parse(siteMap);
+		siteMap.pages = newSitemap;	
+		
+		siteMap = JSON.stringify(siteMap, null, '\t');
+		
+		this.log(siteMap);
+		
+		fs.writeFile('./sitemap.json',
+			siteMap
+		);
+		
 
 	}
 });
+
+
+function pushJson(array,json,path,length,i,pos){
+	
+	if(i == (length)){	
+		array.splice(pos,0,json);
+		return array;
+	}
+	else{
+		pushJson(array[path[i]].childs,json,path,length,i+1,pos);
+		return array
+	}
+}
+
